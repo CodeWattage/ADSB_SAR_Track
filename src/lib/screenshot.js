580d685ecd1bee1e910f9cafd1ada355e7d7efd6
John Watt <johnwatt86@icloud.com>
2023-04-11 14:38:36 +0100

    Modify import.exports again

--- src/lib/screenshot.js
+++ src/lib/screenshot.js
@@ -3,9 +3,9 @@ import chrome from 'selenium-webdriver/chrome';
 import fs from 'fs';
 import path from 'path';
 
-async function takeScreenshot(icao24) {
+async function takeScreenshot(icao24, screenshotDirectory) {
   const url = `https://globe.adsbexchange.com/?icao=${icao24}`;
-  const imagePath = path.join(__dirname, '../../screenshots', `${icao24}.png`);
+  const imagePath = path.join(screenshotDirectory, `${icao24}.png`);
 
   try {
     const options = new chrome.Options();
